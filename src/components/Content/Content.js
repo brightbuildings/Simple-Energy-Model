1f20e5f550dd234c8cc84c709f5dc0b73dc274ff
Scott Borys <scott@territorial.ca>
2021-02-25 13:36:32 -0600

    New Content component

---
+++ src/components/Content/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Content.css";
+
+function Content() {
+  return (
+    <div className="centered-narrow">
+      <div className="Content">
+
+      </div>
+    </div>
+  );
+}
+
+export default Content;
