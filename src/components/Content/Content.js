af8a34bd64a496198297f1a1d4c4155965314ef3
Scott Borys <scott@territorial.ca>
2021-02-26 13:08:36 -0600

    Sections

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,32 +8,61 @@ const formatSections = (data, activeSection) => {
     return;
   }
   return Object.entries(data).map(section => {
-    let key = section[0];
+    const key = section[0];
+    const value = section[1];
+
+    console.log(section);
+
 
     // If Parameters, export both A and B
     if (key === "parameters") {
+      const parametersA = [key+"-a", value];
+      const parametersB = [key+'-b', value];
+
       return (
         <React.Fragment key="parameters">
-          <section key={section[0]+"a"} className={`fieldset ${activeSection === section[0]+"a" ? "active" : ""}`}> 
-            <h2>{section[1].label}</h2>
-            <p>{section[1].content}</p>
-          </section>
-          <section key={section[0]+"b"} className={`fieldset ${activeSection === section[0]+"b" ? "active" : ""}`}>
-            <h2>{section[1].labelB}</h2>
-            <p>{section[1].contentB}</p>
-          </section>
+          {formatSection(parametersA, activeSection)}
+          {formatSection(parametersB, activeSection)}
         </React.Fragment>
-      )
-    } else {
-      return (
-        <section key={section[0]} className={`fieldset ${activeSection === section[0] ? "active" : ""}`}>
-          <h2>{section[1].label}</h2>
-          <p>{section[1].content}</p>
-        </section>
       );
+    } else {
+      return formatSection(section, activeSection);
     }
   });
 };
+
+const formatSection = (section, activeSection) => {
+  const key = section[0];
+  const value = section[1];
+
+  let labelKey = "label";
+  let contentKey = "content";
+  if (key === "parameters-b") {
+    labelKey = "labelB";
+    contentKey = "contentB";
+  }
+  
+  return (
+    <section key={key} className={`fieldset ${activeSection === key ? "active" : ""}`}>
+      <h2>{value[labelKey]}</h2>
+      <p>{value[contentKey]}</p>
+      {Object.entries(value.fields).map(field => {
+        const key = field[0];
+        const value = field[1];
+
+        return (
+          <section key={key}>
+            <h3>{key}</h3>
+            {Object.entries(value).map(input => {
+              return formatQuestion(input);
+            })}
+          </section>
+          );
+        })
+      }
+    </section>
+  );
+}
 
 const formatNavigation = (data, setActiveSection) => {
   if (data == null) {
@@ -46,9 +75,9 @@ const formatNavigation = (data, setActiveSection) => {
     if (key === "parameters") {
       return (
         <React.Fragment key="parameters">
-          <button key={section[0]+"a"} onClick={() => setActiveSection(section[0]+"a")}>Parameters A</button>
+          <button key={section[0]+"a"} onClick={() => setActiveSection(section[0]+"-a")}>Parameters A</button>
           <br />
-          <button key={section[0]+"b"} onClick={() => setActiveSection(section[0]+"b")}>Parameters B</button>
+          <button key={section[0]+"b"} onClick={() => setActiveSection(section[0]+"-b")}>Parameters B</button>
           <br />
         </React.Fragment>
       )
@@ -63,10 +92,25 @@ const formatNavigation = (data, setActiveSection) => {
   });
 };
 
+const formatQuestion = question => {
+
+  switch (question.type) {
+    case "float":
+      break;
+    case "integer":
+      break;
+    case "text":
+      break;
+    case "gap":
+      return <br />;
+    default:
+      break;
+  }
+};
+
 function Content() {
   const [activeSection, setActiveSection] = useState("introduction");
   const data = DataService.load();
-  console.log(Object.entries(data));
 
   return (
     <div className="centered-narrow">
