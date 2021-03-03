6f0ec45eb872b6e0bb5c44bc0aa408d100306dbf
Scott Borys <scott@territorial.ca>
2021-03-03 08:31:18 -0600

    Split parameters into two different inputs

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -9,38 +9,17 @@ const formatSections = (data, activeSection, activeSubsection, setActiveSubsecti
     return;
   }
   return Object.entries(data).map(section => {
-    const [key, value] = section;
-
-    // If Parameters, export both A and B
-    if (key === "parameters") {
-      const parametersA = [key+"-a", value];
-      const parametersB = [key+'-b', value];
-
-      return (
-        <React.Fragment key="parameters">
-          {formatSection(parametersA, activeSection, activeSubsection, setActiveSubsection, variables, setVariables)}
-          {formatSection(parametersB, activeSection, activeSubsection, setActiveSubsection, variables, setVariables)}
-        </React.Fragment>
-      );
-    } else {
-      return formatSection(section, activeSection, activeSubsection, setActiveSubsection, variables, setVariables);
-    }
+    return formatSection(section, activeSection, activeSubsection, setActiveSubsection, variables, setVariables);
   });
 };
 
 const formatSection = (section, activeSection, activeSubsection, setActiveSubsection, variables, setVariables) => {
   const [key, value] = section;
 
-  let labelKey = "label";
-  let contentKey = "content";
-  if (key === "parameters-b") {
-    labelKey = "labelB";
-    contentKey = "contentB";
-  }
   return (
     <FormGroup key={key} className={`fieldset ${activeSection === key ? "active" : ""}`}>
-      <h2>{value[labelKey]}</h2>
-      <p>{value[contentKey]}</p>
+      <h2>{value.label}</h2>
+      <p>{value.content}</p>
 
       <div className="subNavigation">
         {Object.entries(value.fields).map((field, index) => {
@@ -91,43 +70,20 @@ const formatNavigation = (data, activeSection, setActiveSection) => {
   if (data == null) {
     return;
   }
-  return Object.keys(data).map(key => {
+  return Object.entries(data).map(navItem => {
+    const [key, value] = navItem;
 
-    // If Parameters, export both A and B
-    if (key === "parameters") {
-      return (
-        <React.Fragment key="parameters">
-          <button 
-            className={`${key}-a` === activeSection ? "active" : "" } 
-            key={key+"a"} 
-            onClick={() => setActiveSection(key+"-a")}
-          >
-            Parameters A
-          </button>
-          <br />
-          <button 
-            className={`${key}-b` === activeSection ? "active" : "" } 
-            key={key+"b"} 
-            onClick={() => setActiveSection(key+"-b")}
-          >
-            Parameters B
-          </button>
-          <br />
-        </React.Fragment>
-      )
-    } else {
-      return (
-        <React.Fragment key={key}>
-          <button
-            className={key === activeSection ? "active" : "" } 
-            onClick={() => setActiveSection(key)}
-          >
-            {key}
-          </button>
-          <br />
-        </React.Fragment>
-      );
-    }
+    return (
+      <React.Fragment key={key}>
+        <button
+          className={key === activeSection ? "active" : "" } 
+          onClick={() => setActiveSection(key)}
+        >
+          {value.label}
+        </button>
+        <br />
+      </React.Fragment>
+    );
   });
 };
 
@@ -179,6 +135,8 @@ const formatQuestion = (question, variables, setVariables) => {
           )}
         </InputGroup>
       );
+    case "select":
+      return "TODO";
     case "gap":
       return <div key={key} className="spacer">&nbsp;</div>;
     default:
