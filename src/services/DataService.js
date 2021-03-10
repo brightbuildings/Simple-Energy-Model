76925ab64084c47d4c41516dcd94ad5a906ca953
Scott Borys <scott@territorial.ca>
2021-03-10 13:10:09 -0600

    Method to create flat array of object values

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -4,6 +4,27 @@ function load() {
   return data;
 }
 
+function getAllOptionsObjects() {
+  const data = load();
+  const optionObjects = [];
+  Object.entries(data).forEach(i => {
+    if (i[1].fields) {
+      Object.entries(i[1].fields).forEach(j => {
+        if (j[1].fields) {
+          Object.entries(j[1].fields).forEach(k => {
+            const [key, value] = k;
+            if (value.type === "select") {
+              optionObjects[key] = value;
+            }
+          });
+        }
+      })
+    }
+  });
+  return optionObjects;
+}
+
 module.exports = {
-  load
+  load,
+  getAllOptionsObjects,
 };
