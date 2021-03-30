365185414fbe2dfabe064a7a32710687c8277524
Scott Borys <scott@territorial.ca>
2021-03-30 07:23:11 -0600

    Sentry setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,30 @@
 import React from "react";
+import * as Sentry from "@sentry/browser";
+import { Button } from "reactstrap";
 
 import "./Header.css";
+
+function triggerSentry() {
+  Sentry.captureException("Manual Feedback");
+  Sentry.showReportDialog({
+    title: "Feedback",
+    subtitle: "Please describe your bug or your thoughts on how we can improve the app.",
+    subtitle2: "",
+    labelSubmit: "Submit"
+  });
+}
 
 function Header() {
   return (
     <div className="Header">
-      <div className="centered">
-        <h1><a href="https://brightbuildings.tumblr.com/">Bright Buildings</a></h1>
-        <h3>Simple Energy Model</h3>
+      <div className="centered Navigation">
+        <div>
+          <h1><a href="https://brightbuildings.tumblr.com/">Bright Buildings</a></h1>
+          <h3>Simple Energy Model</h3>
+        </div>
+        <div>
+          <Button onClick={triggerSentry} className="FeedbackButton">Feedback</Button>
+        </div>
       </div>
     </div>
   );
